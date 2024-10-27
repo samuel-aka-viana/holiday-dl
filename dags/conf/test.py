840e6856db6fe247cc6b8790400a606251dfab71
Samuel Viana da Silva <samuelviana2626@gmail.com>
2024-10-27 18:31:52 -0400

    get landing to minIO

--- dags/conf/test.py
+++ dags/conf/test.py
@@ -1,7 +1,27 @@
+from pyspark.sql import SparkSession
 
-extractor = HolidayExtractor(2024)
-national = extractor.get_national_holidays()
-state = extractor.get_state_holidays()
+# Configuração do Spark
+spark = SparkSession.builder \
+    .appName("MinIO Example") \
+    .config("spark.hadoop.fs.s3a.access.key", "DUMMYIDEXAMPLE") \
+    .config("spark.hadoop.fs.s3a.secret.key", "DUMMYEXAMPLEKEY") \
+    .config("spark.hadoop.fs.s3a.endpoint", "http://localhost:9000") \
+    .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+    .getOrCreate()
 
-print(f"Nacionais: {len(national)}")
-print(f"Estaduais: {len(state)}")
+# Exemplo de dados
+data = [("Alice", 34), ("Bob", 45), ("Cathy", 29)]
+columns = ["Name", "Age"]
+
+# Criando um DataFrame
+df = spark.createDataFrame(data, columns)
+
+# Escrevendo no MinIO
+df.write.csv("s3a://datalake/people.csv", header=True)
+
+# Lendo do MinIO
+df_read = spark.read.csv("s3a://datalake/people.csv", header=True)
+df_read.show()
+
+# Finalizando a sessão Spark
+spark.stop()
